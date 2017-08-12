9ca8f04bf20f21a3e7e2c76204150acf99910c66
Futa Hirakoba <korosuke613613@gmail.com>
2017-08-12 21:55:32 +0900

    Modify DataBaseControl test

--- test/test_database.py
+++ test/test_database.py
@@ -12,7 +12,17 @@ def test_open_db_1():
     assert value is True
 
 
-def test_open_db_2():
-    with pytest.raises(psycopg2.Error):
-        db = DatabaseControl(os.environ["DATABASE_URL"])
-        db.sql_execute("SELECT * FROM noting")
+def test_sql_execute1():
+    db = DatabaseControl(os.environ["DATABASE_URL"])
+    value = db.sql_execute("SELECT * FROM noting")
+    db.close_connect()
+
+    assert value is False
+
+
+def test_sql_execute2():
+    db = DatabaseControl(os.environ["DATABASE_URL"])
+    value = db.sql_execute("SELECT * FROM miyadai_shienka_news")
+    db.close_connect()
+
+    assert value is True
