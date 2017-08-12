920c5d772831ac3ddec478fb53d0fd5020b8d7a2
Futa Hirakoba <korosuke613613@gmail.com>
2017-08-12 21:53:44 +0900

    Create fetch_all_method

--- app/myModules/DatabaseControl.py
+++ app/myModules/DatabaseControl.py
@@ -4,31 +4,36 @@ import psycopg2
 
 class DatabaseControl:
     def __init__(self, db_url_):
-        self._database_url = urllib.parse.urlparse(db_url_)
-        self.conn = psycopg2.connect(
-            database=self._database_url.path[1:],
-            user=self._database_url.username,
-            password=self._database_url.password,
-            host=self._database_url.hostname,
-            port=self._database_url.port
+        self.__database_url = urllib.parse.urlparse(db_url_)
+        self.__conn = psycopg2.connect(
+            database=self.__database_url.path[1:],
+            user=self.__database_url.username,
+            password=self.__database_url.password,
+            host=self.__database_url.hostname,
+            port=self.__database_url.port
         )
-        self.cur = self.conn.cursor()
+        self.__cur = self.__conn.cursor()
 
     def close_connect(self):
-        self.cur.close()
-        self.conn.close()
+        self.__cur.close()
+        self.__conn.close()
         return True
 
     def sql_execute(self, sql):
         try:
-            self.cur.execute(sql)
-        except psycopg2.Error as e:
-            print(e)
-            raise
+            self.__cur.execute(sql)
+        except psycopg2.Error:
+            return False
+        else:
+            return True
 
     def sql_fetch_one(self):
         # TODO 例外を作る
-        return self.cur.fetchone()
+        return self.__cur.fetchone()
+
+    def sql_fetch_all(self):
+        # TODO 例外を作る
+        return self.__cur.fetchall()
 
     def commit(self):
-        self.conn.commit()
+        self.__conn.commit()
