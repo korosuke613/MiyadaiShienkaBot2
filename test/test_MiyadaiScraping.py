36fe3987d2015fffef336119336f65b9a4f1b3ad
Futa Hirakoba <korosuke613613@gmail.com>
2017-08-14 11:37:18 +0900

    Add test convert

--- test/test_MiyadaiScraping.py
+++ test/test_MiyadaiScraping.py
@@ -1,3 +1,4 @@
+import pytest
 from app.MiyadaiScraping import MiyadaiScraping
 import os.path
 
@@ -31,7 +32,20 @@ def test_check_pdf_2():
     assert url is None
 
 
-def test_screenshot_news_crop1():
+@pytest.fixture()
+def fixture_rm_screenshot():
+    if os.path.isfile("screenshot.png") is True:
+        os.remove("screenshot.png")
+    if os.path.isfile("screenshot_crop.png") is True:
+        os.remove("screenshot_crop.png")
+    yield True
+    if os.path.isfile("screenshot.png") is True:
+        os.remove("screenshot.png")
+    if os.path.isfile("screenshot_crop.png") is True:
+        os.remove("screenshot_crop.png")
+
+
+def test_screenshot_news_crop1(fixture_rm_screenshot):
     sc = MiyadaiScraping()
     url = "http://gakumu.of.miyazaki-u.ac.jp/gakumu/andsoon/andsoon/3508-2017-08-10-04-08-15.html"
     assert sc.screenshot_news_crop(url) is True
@@ -40,7 +54,7 @@ def test_screenshot_news_crop1():
     assert os.path.isfile("screenshot_crop.png")
 
 
-def test_screenshot_news_crop2():
+def test_screenshot_news_crop2(fixture_rm_screenshot):
     sc = MiyadaiScraping()
     url = sc.check_pdf("http://gakumu.of.miyazaki-u.ac.jp/gakumu/andsoon/andsoon/3508-2017-08-10-04-08-15.html")
     assert sc.screenshot_news_crop(url) is False
--- test/test_pdf2png.py
+++ test/test_pdf2png.py
@@ -1,7 +1,7 @@
 import os.path
 import pytest
 
-from app.myModules.pdf2png import download_pdf
+from app.myModules.pdf2png import download_pdf, convert
 
 url = "http://gakumu.of.miyazaki-u.ac.jp/gakumu/images/campuslife/scholarship/H29/ooame20170705.pdf"
 not_url = "http://gakumu.of.miyazaki-u.ac.jp/gakumu/images/campuslife/scholarship/H29/"
@@ -11,9 +11,13 @@ not_url = "http://gakumu.of.miyazaki-u.ac.jp/gakumu/images/campuslife/scholarshi
 def fixture_rm_pdf():
     if os.path.isfile("test.pdf") is True:
         os.remove("test.pdf")
+    if os.path.isfile("test.png") is True:
+        os.remove("test.png")
     yield True
     if os.path.isfile("test.pdf") is True:
         os.remove("test.pdf")
+    if os.path.isfile("test.png") is True:
+        os.remove("test.png")
 
 
 def test_download_pdf1(fixture_rm_pdf):
@@ -22,6 +26,12 @@ def test_download_pdf1(fixture_rm_pdf):
     assert os.path.isfile("test.pdf")
 
 
-#def test_download_pdf2(fixture_rm_pdf):
+def test_convert1(fixture_rm_pdf):
+    download_pdf(url, "test.pdf")
+    convert("test.pdf")
+    assert os.path.isfile("test.png")
+
+
+# def test_download_pdf2(fixture_rm_pdf):
 #    download_pdf(not_url, "test.pdf")
 #    assert os.path.isfile("test.pdf") is False
