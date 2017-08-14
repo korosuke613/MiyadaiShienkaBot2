1b93b9f01eb487a92a2d9b677c57a9b1ecefb7a9
Futa Hirakoba <korosuke613613@gmail.com>
2017-08-14 16:54:36 +0900

    Add test convert

--- app/upload.py
+++ app/upload.py
@@ -1,22 +1,22 @@
 import os
 import urllib3
 import cloudinary
-from cloudinary.uploader import upload as cloud_upload
+from cloudinary.uploader import upload
 
 
-urllib3.disable_warnings()
+def init_upload():
+    urllib3.disable_warnings()
 
-cloudinary.config(
-    cloud_name=os.environ["CLOUDINARY_CLOUD_NAME"],
-    api_key=os.environ["CLOUDINARY_API_KEY"],
-    api_secret=os.environ["CLOUDINARY_API_SECRET"],
-)
+    cloudinary.config(
+        cloud_name=os.environ["CLOUDINARY_CLOUD_NAME"],
+        api_key=os.environ["CLOUDINARY_API_KEY"],
+        api_secret=os.environ["CLOUDINARY_API_SECRET"],
+    )
 
 
 def upload_image(filename, tag):
-    dic = cloud_upload(filename, tags=tag)
+    dic = upload(filename, tags=tag)
     return dic['url']
 
 
-if __name__ == "__main__":
-    upload_image("screenshot_crop.png", "miyadai-shienka-screen")
+init_upload()
