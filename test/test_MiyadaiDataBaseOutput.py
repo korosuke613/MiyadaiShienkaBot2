3f87992593600d2cb252d2e22eead5e443ee1d7b
Futa Hirakoba <korosuke613613@gmail.com>
2017-08-20 21:50:06 +0900

    Add test

--- test/test_MiyadaiDataBaseOutput.py
+++ test/test_MiyadaiDataBaseOutput.py
@@ -25,3 +25,10 @@ def test_fetch_shienka_news_once1(fixture_db):
     assert dic["day"] == "2017年08月10日"
     assert dic["title"] == "学生ボランティア活動支援室ホームページ立ち上げのお知らせ"
     assert dic["url_news"] == "http://gakumu.of.miyazaki-u.ac.jp/gakumu/andsoon/andsoon/3507-2017-08-10-02-47-07.html"
+
+
+def test_fetch_shienka_news_once2(fixture_db):
+    dic = fixture_db.fetch_shienka_news_once(
+        "http://ppp-lab.sakura.ne.jp/ProgrammingPlacePlus/cpp/index.html")
+
+    assert dic is False
